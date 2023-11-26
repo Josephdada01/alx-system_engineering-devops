73aaaf99d903dbc0fedfe1064ed8919be25dad69
Josephdada01 <ridwandada500@gmail.com>
2023-11-26 12:50:57 -0800

    checking requirements

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-#Using Puppet, create a file in /tmp.
-#Requirements:
-#File path is /tmp/school
-#File permission is 0744
-#File owner is www-data
-#File group is www-data
-#File contains I love Puppet
+# Using Puppet, create a file in /tmp.
+# Requirements:
+# File path is /tmp/school
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet
 
 file { '/tmp/school':
     ensure  => 'file',
