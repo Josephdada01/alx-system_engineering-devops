5894472a94e1751474c24c9594e07e84d2d78267
Josephdada01 <ridwandada500@gmail.com>
2023-11-26 21:26:15 +0100

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,15 @@
+#Using Puppet, create a file in /tmp.
+#Requirements:
+#File path is /tmp/school
+#File permission is 0744
+#File owner is www-data
+#File group is www-data
+#File contains I love Puppet
+
+file { '/tmp/school':
+    ensure  => 'file',
+    content => 'I love puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+}
