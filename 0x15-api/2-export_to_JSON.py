82d54bd86433e26bf576dfef48ddb2ff0e610670
Josephdada01 <ridwandada500@gmail.com>
2024-01-30 15:35:05 +0100

    Task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -39,18 +39,20 @@ def get_employee_todo_progress(employee_id):
                 for task in completed_tasks:
                     print(f'\t{task["title"]}')
 
-                """Exporting the data gathered to json file"""
                 json_filename = f'{employee_id}.json'
                 with open(json_filename, 'w') as json_file:
-                    json_data = [
-                        {
-                            "task": task["title"],
-                            "completed": task["completed"],
-                            "username": employee_name
-                        }
-                        for task in todo_data
-                    ]
-                    json.dump({"USER_ID": json_data}, json_file, indent=2)
+                    json_data = []
+                    for task in todo_data:
+                        task_obj = {
+                            "task": task.get('title'),
+                            "completed": task.get('completed'),
+                            "username": employee_name,
+                            }
+                        json_data.append(task_obj)
+                    """Check if the last task object is empty&remove it"""
+                    if json_data and not any(json_data[-1].values()):
+                        json_data.pop()
+                        json.dump({"USER_ID": json_data}, json_file, indent=2)
 
     except urllib.error.HTTPError as e:
         print(e)
