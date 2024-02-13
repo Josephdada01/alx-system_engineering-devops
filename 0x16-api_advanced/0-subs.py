718e3d0d2c3466cba698c5215c118eaa5551ceb4
Josephdada01 <ridwandada500@gmail.com>
2024-02-13 21:20:49 +0100

    uing the get request to find number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" How many subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    a function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """setting a custom User-Agent in case of too many request"""
+    headers = {'User-agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            return 0
+    except BaseException:
+        return 0
