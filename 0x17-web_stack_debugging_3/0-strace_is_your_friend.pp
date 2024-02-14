944248761535957036798bdfcc88ba03191a9093
Josephdada01 <ridwandada500@gmail.com>
2024-02-14 08:26:27 +0100

    ensuring it passes pupepet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Using strace to find out why Apache is returning a 500 error.
+# Using strace to find out why Apache is returning 500 error.
 
 exec {'modification':
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
